4479df4ca7b406a5069e09b7a6db60e298e384e8
Brian Noh <noh24@ymail.com>
2022-11-15 17:09:33 -0500

    add get giphy function
    Co-authored-by: Aitana Shough <aeshough@gmail.com>
    Co-authored-by: Richelle Thorpe <richelle.ak@gmail.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,65 @@
-//business logic
-
-function getWeather(city) {
+// business logic
+//search GIPHY api function
+function getGiphy(userSearchQuery) { 
   let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userSearchQuery}&limit=25&offset=0&rating=g&lang=en`;
+  
   request.addEventListener('loadend', function() {
-    const response = JSON.parse(this.responseText);
+    let response = JSON.parse(this.responseText);
     if (this.status === 200) {
-      printElements(response, city);
+      printElement(response);
     } else {
-      printError(this, response, city);
+      printError(this, response, userSearchQuery);
     }
   });
-
+  
   request.open("GET", url, true);
   request.send();
 }
+//trendy gif
+// function trendyGiphy() {
+//   let request = new XMLHttpRequest();
+//   const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=25&rating=r`;
 
-//UI Logic
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+//   request.addEventListener('loadend', function() {
+//     let response = JSON.parse(this.responseText);
+//     if (this.status === 200) {
+//       printElement(response);
+//     } else {
+//       printError(this, response);
+//     }
+//   });
+
+//   request.open("GET", url, true);
+//   request.send();
+// }
+// ui logic
+function printError(request, apiResponse, search) {
+  document.getElementById('response').innerHTML = null;
+  document.getElementById('response').innerHTML = `${search} is not a valid search. ${request.status} ${request.statusText}: ${apiResponse.meta.msg}`;
 }
-function printElements(apiResponse,city) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
-    The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+
+function printElement(apiResponse) {
+  document.getElementById('response').innerHTML = null; 
+  document.getElementById('gif-input').value = null;
+
+  apiResponse.data.forEach(gif => {
+    const img = document.createElement('img');
+    img.setAttribute('src', gif.images.original.url);
+    img.setAttribute('width', '150px');
+    document.getElementById('response').append(img);
+  });
 }
 
-function handleFormSubmission(event) {
-  event.preventDefault();
-  const city = document.querySelector('#location').value;
-  document.querySelector('#location').value = null;
-  getWeather(city);
-}
+// function printError(apiResponse) {
+  
+// }
 
-
-document.querySelector('form').addEventListener('submit', handleFormSubmission);
+window.addEventListener('load', function() {
+  // document.getElementById('trendy').addEventListener('click', trendyGiphy);
+  document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    let search = document.getElementById('gif-input').value;
+    getGiphy(search);
+  });
+});
