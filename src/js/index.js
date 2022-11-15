795f909cb16e027d2960ba8db7cabaa7ccf2c186
Brian <106576147+noh24@users.noreply.github.com>
2022-11-15 13:36:14 -0500

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+//business logic
+
+function getWeather(city) {
+  let request = new XMLHttpRequest();
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+
+  request.addEventListener('loadend', function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response, city);
+    } else {
+      printError(this, response, city);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
+
+//UI Logic
+function printError(request, apiResponse, city) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+}
+function printElements(apiResponse,city) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
+    The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const city = document.querySelector('#location').value;
+  document.querySelector('#location').value = null;
+  getWeather(city);
+}
+
+
+document.querySelector('form').addEventListener('submit', handleFormSubmission);
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  devtool: 'eval-source-map',
+  plugins: [
+    new Dotenv(),
+    new ESLintPlugin(),
+    new CleanWebpackPlugin({
+      verbose: true
+    }),
+    new HtmlWebpackPlugin({
+      title: 'weather-api',
+      template: './src/html/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(gif|png|avif|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader'
+        ]
+      }
+    ]
+  }
+};
