856a64f30c660cb392722c039ce1816d6da581e8
Brian Noh <noh24@ymail.com>
2022-11-16 23:09:39 -0500

    add fetch api and async functions

--- src/js/giphy-service.js
+++ src/js/giphy-service.js
@@ -1,49 +1,131 @@
-export class GiphyService {
-  static getGiphy(userSearchQuery) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userSearchQuery}&limit=25&offset=0&rating=r&lang=en`;
+// export class GiphyService {
+//   static getGiphy(userSearchQuery) {
+//     return new Promise(function(resolve, reject) {
+//       let request = new XMLHttpRequest();
+//       let url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userSearchQuery}&limit=25&offset=0&rating=r&lang=en`;
       
-      request.addEventListener('loadend', function() {
-        let response = JSON.parse(this.responseText);
-        if (this.status === 200) {
-          resolve(response);
+//       request.addEventListener('loadend', function() {
+//         let response = JSON.parse(this.responseText);
+//         if (this.status === 200) {
+//           resolve(response);
+//         } else {
+//           reject([this, response, userSearchQuery]);
+//         }
+//       });
+//       request.open("GET", url, true);
+//       request.send();
+//     });
+//   }
+// }
+
+// export class TrendyGiphyService {
+//   //create static method
+//   static trendyGiphy() {
+//     //return new promise object
+//     return new Promise(function(resolve, reject) {
+//       // create request object
+//       let request = new XMLHttpRequest();
+//       // create a url variable
+//       let url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=25&rating=r`;
+  
+//       //open request (method, url, async)
+//       request.open('GET', url, true);
+//       //send request
+//       request.send();
+  
+//       //add event listener to request to wait for sent request to finish
+//       request.addEventListener('loadend', function() {
+//         //JSON parse the response you get from the request
+//         let response = JSON.parse(this.responseText);
+//         // conditional to determine what is resolve or rejected promise
+//         if (this.status === 200) {
+//           resolve(response);
+//         } else {
+//           reject([this, response]);
+//         }
+//       });
+//     });
+//   }
+// }
+
+// export class RandomGiphyService {
+//   static getRandomGiphy() {
+//     return fetch(`https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=pg`)
+//       .then(response => {
+//         if (!response.ok) {
+//           const errorMsg = `${response.status} ${response.statusText}`;
+//           throw new Error(errorMsg);
+//         } else {
+//           return response.json();
+//         }
+//       })
+//       .catch(error => {
+//         return error;
+//       });
+//   }
+// }
+
+// export class GetGiphyService {
+//   static getGiphy(search) {
+//     return fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${search}&limit=25&offset=0&rating=r&lang=en`)
+//       .then(response => {
+//         if (!response.ok) {
+//           const errorMsg = `${response.status} ${response.statusText}`;
+//           throw new Error(errorMsg);
+//         } else {
+//           return response.json();
+//         }
+//       })
+//       .catch(error => error);
+//   }
+// }
+
+export class TrendyGiphyService {
+  static trendyGiphy() {
+    return fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=25&rating=r`)
+      .then(response => {
+        if (!response.ok) {
+          const errorMsg = `${response.status} ${response.statusText}`;
+          throw new Error(errorMsg);
         } else {
-          reject([this, response, userSearchQuery]);
+          return response.json();
         }
-      });
-      request.open("GET", url, true);
-      request.send();
-    });
+      })
+      .catch(error => error);
   }
 }
 
-export class TrendyGiphyService {
-  //create static method
-  static trendyGiphy() {
-    //return new promise object
-    return new Promise(function(resolve, reject) {
-      // create request object
-      let request = new XMLHttpRequest();
-      // create a url variable
-      let url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=25&rating=r`;
-  
-      //open request (method, url, async)
-      request.open('GET', url, true);
-      //send request
-      request.send();
-  
-      //add event listener to request to wait for sent request to finish
-      request.addEventListener('loadend', function() {
-        //JSON parse the response you get from the request
-        let response = JSON.parse(this.responseText);
-        // conditional to determine what is resolve or rejected promise
-        if (this.status === 200) {
-          resolve(response);
-        } else {
-          reject([this, response]);
-        }
-      });
-    });
+// random giphy
+export class RandomGiphyService {
+  static async randomGiphy() {
+    try {
+      let response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=pg`);
+      let jsonResponse = await response.json();
+
+      if (!response.ok) {
+        const errorMsg = `${response.status} ${response.statusText} ${jsonResponse.meta.msg}`;
+        throw new Error(errorMsg);
+      }
+      return jsonResponse;
+    } catch(error) {
+      return error;
+    }
+  }
+}
+
+export class GetGiphyService {
+  static async getGiphy(search) {
+    try {
+      let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${search}&limit=25&offset=0&rating=r&lang=en`);
+      let jsonResponse = await response.json();
+
+      if (!response.ok) {
+        const errorMsg = `${response.status} ${response.statusText} ${jsonResponse.meta.msg}`;
+        throw new Error(errorMsg);
+      }
+      return jsonResponse;
+    } catch(error) {
+      return error;
+    }
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,76 +1,151 @@
-import { GiphyService, TrendyGiphyService } from "./giphy-service";
+import { GetGiphyService, TrendyGiphyService, RandomGiphyService } from "./giphy-service";
+// import { GetGiphyService, RandomGiphy } from './giphy-service.js';
 //search GIPHY api function
-function getGiphy(userSearchQuery) { 
-  let promise = GiphyService.getGiphy(userSearchQuery);
+// function getGiphy(userSearchQuery) { 
+//   let promise = GiphyService.getGiphy(userSearchQuery);
 
-  promise.then(function(giphyData) {
-    printElement(giphyData);
-  }, function(giphyErrorArray) {
-    printError(giphyErrorArray);
-  });
-}
-// trendy gif
-
-function trendyGiphy() {
-  let promise = TrendyGiphyService.trendyGiphy();
-  
-  promise.then(function(trendyGiphyData) {
-    printElement(trendyGiphyData);
-  }, function(trendyGiphyErrorArray) {
-    printError(trendyGiphyErrorArray);
-  });
-}
-// // random gif
-// function randomGiphy() {
-//   let request = new XMLHttpRequest();
-//   const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=pg`;
-
-//   request.addEventListener('loadend', function() {
-//     let response = JSON.parse(this.responseText);
-//     if (this.status === 200) {
-//       printElement(response);
-//     } else {
-//       printError(this, response);
-//     }
+//   promise.then(function(giphyData) {
+//     printElement(giphyData);
+//   }, function(giphyErrorArray) {
+//     printError(giphyErrorArray);
 //   });
-
-//   request.open("GET", url, true); 
-//   request.send();
 // }
 
-// ui logic
+// trendy gif
 
-function printElement(data) {
+// function trendyGiphy() {
+//   let promise = TrendyGiphyService.trendyGiphy();
+  
+//   promise.then(function(trendyGiphyData) {
+//     printElement(trendyGiphyData);
+//   }, function(trendyGiphyErrorArray) {
+//     printError(trendyGiphyErrorArray);
+//   });
+// }
+
+//random giphy
+//async function
+async function randomGiphy() {
+  let response = await RandomGiphyService.randomGiphy();
+  if (response.data) {
+    printElement(response);
+  } else {
+    printError(response);
+  }
+}
+
+// fetch api
+// function randomGiphy() {
+//   RandomGiphyService.getRandomGiphy()
+//     .then(response => {
+//       if (response.data) {
+//         printElement(response);
+//       } else {
+//         printError(response);
+//       }
+//     });
+// }
+
+//get giphy
+//async function
+async function getGiphy(search) {
+  let response = await GetGiphyService.getGiphy(search);
+  if (response.data) {
+    printElement(response);
+  } else {
+    printError(response, search);
+  }
+}
+// //fetch api
+// function getGiphy(search) {
+//   GetGiphyService.getGiphy(search)
+//     .then(response => {
+//       if (response.data) {
+//         printElement(response);
+//       } else {
+//         printError(response, search);
+//       }
+//     });
+// }
+
+//trendy
+function trendyGiphy() {
+  TrendyGiphyService.trendyGiphy()
+    .then(response => {
+      if (response.data) {
+        printElement(response);
+      } else {
+        printError(response);
+      }
+    });
+}
+
+
+// ui logic
+//fetch
+
+function printElement(response) {
   document.getElementById('response').innerHTML = null; 
   document.getElementById('gif-input').value = null;
   
-  if (data.data[1] !== undefined) {
-    data.data.forEach(gif => {
+  if (response.data[1] !== undefined) {
+    response.data.forEach(gif => {
       const img = document.createElement('img');
       img.setAttribute('src', gif.images.original.url);
       img.setAttribute('width', '250px');
       document.getElementById('response').append(img);
     });
-  } else {
+  } else if (response.data[0] !== undefined) {
     const img = document.createElement('img');
-    img.setAttribute('src', data.data.images.original.url);
+    img.setAttribute('src', response.data.images.original.url);
     document.getElementById('response').append(img);
-  }
-}
-
-function printError(error) {
-  document.getElementById('response').innerHTML = null;
-  if (error[2] !== undefined) {
-    document.getElementById('response').innerHTML = `${error[2]} is not a valid search. ${error[0].status} ${error[0].statusText}: ${error[1].meta.msg}`;
   } else {
-    document.getElementById('response').innerHTML = `${error[0].status} ${error[0].statusText}: ${error[1].meta.msg}`;
-
+    const p = document.createElement('p');
+    p.innerText = `Enter a valid search`;
+    p.style.color = 'red';
+    document.getElementById('response').append(p);
   }
 }
+
+function printError(error, search) {
+  document.getElementById('response').innerHTML = null;
+  if (search) {
+    document.getElementById('response').innerHTML = `${search} is not a valid search. ${error}`;
+  } else {
+    document.getElementById('response').innerHTML = `${error}`;
+  }
+}
+// // promises
+// function printElement(data) {
+//   document.getElementById('response').innerHTML = null; 
+//   document.getElementById('gif-input').value = null;
+  
+//   if (data.data[1] !== undefined) {
+//     data.data.forEach(gif => {
+//       const img = document.createElement('img');
+//       img.setAttribute('src', gif.images.original.url);
+//       img.setAttribute('width', '250px');
+//       document.getElementById('response').append(img);
+//     });
+//   } else {
+//     const img = document.createElement('img');
+//     img.setAttribute('src', data.data.images.original.url);
+//     document.getElementById('response').append(img);
+//   }
+// }
+
+// function printError(error) {
+//   document.getElementById('response').innerHTML = null;
+//   if (error[2] !== undefined) {
+//     document.getElementById('response').innerHTML = `${error[2]} is not a valid search. ${error[0].status} ${error[0].statusText}: ${error[1].meta.msg}`;
+//   } else {
+//     document.getElementById('response').innerHTML = `${error[0].status} ${error[0].statusText}: ${error[1].meta.msg}`;
+//   }
+// }
   
 window.addEventListener('load', function() {
   document.getElementById('trendy').addEventListener('click', trendyGiphy);
-  // document.getElementById('random').addEventListener('click', randomGiphy);
+  document.getElementById('random').addEventListener('click', randomGiphy);
   document.getElementById('form').addEventListener('submit', function(e) {
     e.preventDefault();
     let search = document.getElementById('gif-input').value;
